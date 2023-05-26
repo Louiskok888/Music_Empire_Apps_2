49b3347c87ade646939bf30405ba2ed4cbc628cd
Louiskok888 <115003571+Louiskok888@users.noreply.github.com>
2023-05-26 17:06:47 +0800

    package json and next config modified

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Music_Empire_Apps_2' : '',
 };
 
 // const withTM = require('next-transpile-modules')(['fs-module'])
